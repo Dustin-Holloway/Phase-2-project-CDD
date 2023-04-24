fa18b581cb5edf219ec87387e54d65cb3ed5ab2d
Dustin <dustinhollowaypdx@gmail.com>
2023-04-24 12:44:57 -0700

    flex-box

--- phase-2-group-project/src/App.js
+++ phase-2-group-project/src/App.js
@@ -2,15 +2,19 @@ import "./App.css";
 import Header from "./components/header";
 import Form from "./components/form";
 import { Switch, Route } from "react-router-dom";
+import CardContainer from "./components/card_container";
 
 function App() {
   return (
     <div>
       <Header />
       <Switch>
-        <Route path="/Gallery">
+        <Route exact path="/">
           <h1>Phase-2 Group Project</h1>
           <h3>Cahen, Dominick, Dustin</h3>
+        </Route>
+        <Route path="/Gallery">
+          <CardContainer className="card_container" />
         </Route>
 
         <Route exact path="/Game">
