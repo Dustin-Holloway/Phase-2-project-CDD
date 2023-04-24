949a3c4504e4c6c97f1d1958734a30a11dc01bb1
Dustin <dustinhollowaypdx@gmail.com>
2023-04-24 11:45:35 -0700

    added Switch and Routes

--- phase-2-group-project/src/App.js
+++ phase-2-group-project/src/App.js
@@ -1,12 +1,22 @@
 import "./App.css";
 import Header from "./components/header";
+import Form from "./components/form";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Header />
-      <h1>Phase-2 Group Project</h1>
-      <h3>Cahen, Dominick, Dustin</h3>
+      <Switch>
+        <Route path="/Gallery">
+          <h1>Phase-2 Group Project</h1>
+          <h3>Cahen, Dominick, Dustin</h3>
+        </Route>
+
+        <Route exact path="/Game">
+          <Form />
+        </Route>
+      </Switch>
     </div>
   );
 }
